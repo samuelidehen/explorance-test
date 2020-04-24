51802deaf5897243ebee83f41e8a5d7e58f8011f
Samuel Idehen <samuelidehenlion@gmail.com>
2020-04-24 04:25:19 -0400

    -- bug fic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MaterialModule } from "./material/material.module";
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
+
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { UsersComponent } from "./components/users/users.component";
 import { UserComponent } from "./components/users/user/user.component";
@@ -11,20 +11,11 @@ import { UserService } from "./shared/user.service";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
+import { environment } from "../environments/environment";
 import { MatDialogRef } from "@angular/material";
 import { DatePipe } from "@angular/common";
 import { UserListComponent } from "./components/users/user-list/user-list.component";
-const firbaseConfig = {
-  firbaseConfig: {
-    apiKey: "AIzaSyCcTdEITJdh7bRtbExESU3IboGLjozYvgI",
-    authDomain: "explorance-form.firebaseapp.com",
-    databaseURL: "https://explorance-form.firebaseio.com",
-    projectId: "explorance-form",
-    storageBucket: "explorance-form.appspot.com",
-    messagingSenderId: "815802272792",
-    appId: "1:815802272792:web:4dca7dc5f2c0b86a3a876d"
-  }
-};
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -34,11 +25,11 @@ const firbaseConfig = {
     NgbModule,
     MaterialModule,
     AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(firbaseConfig.firbaseConfig)
+    AngularFireModule.initializeApp(environment.firbaseConfig)
   ],
   declarations: [
     AppComponent,
-    HelloComponent,
+
     UsersComponent,
     UserComponent,
     UserListComponent
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firbaseConfig: {
+    apiKey: "AIzaSyCcTdEITJdh7bRtbExESU3IboGLjozYvgI",
+    authDomain: "explorance-form.firebaseapp.com",
+    databaseURL: "https://explorance-form.firebaseio.com",
+    projectId: "explorance-form",
+    storageBucket: "explorance-form.appspot.com",
+    messagingSenderId: "815802272792",
+    appId: "1:815802272792:web:4dca7dc5f2c0b86a3a876d"
+  }
+};
